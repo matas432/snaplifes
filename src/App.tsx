88ae5e5b286eff3c1dd765009f7e690ce36108f6
Matthias Malek <matthiasmalek72@gmail.com>
2025-03-31 10:50:17 +0900

    change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  export default function App() {
    return (
     <div className="flex justify-center items-center h-screen">
-       <h1 className="text-center">Hello</h1>
+       <h1 className="text-center">Hello 2</h1>
     </div>
    )
  }
